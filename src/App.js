7cf8e07a4c87c2d6120d2732de3f4667173c6417
Jonathan Tom <Jonathan.Y.Tom1@gmail.com>
2017-08-14 15:07:33 -0700

    adding css

--- src/App.js
+++ src/App.js
@@ -4,7 +4,10 @@ import './App.css';
 class App extends Component {
   constructor () {
     super();
-    this.state = ({outputText: ''});
+    this.state = ({
+      outputText: '',
+      previousCalculations: ''
+    });
     this.addText = this.addText.bind(this);
     this.equals = this.equals.bind(this);
     this.AC = this.AC.bind(this);
@@ -19,55 +22,72 @@ class App extends Component {
     let lastIndex = this.state.outputText.length-1;
     if (s === '+' || s === '-' || s === '/' || s === '*') {
       if (this.state.outputText[lastIndex] !== '+' && this.state.outputText[lastIndex] !== '-' && this.state.outputText[lastIndex] !== '*' && this.state.outputText[lastIndex] !== '/' && this.state.outputText !== '' ) {
-        // outputText += s;
         this.setState({outputText: this.state.outputText += s});
       }
     } else {
-      // outputText+=s;
       this.setState({outputText: this.state.outputText += s});
     }
     document.getElementById('input').value = this.state.outputText;
   }
 
-  equals (a) {
-    document.getElementById('input').value = eval(a);
-    // outputText = '';
-    this.setState({outputText: ''});    
+  equals () {
+    if (this.state.outputText[this.state.outputText.length-1] === '+' || this.state.outputText[this.state.outputText.length-1] === '-' || this.state.outputText[this.state.outputText.length-1] === '/' || this.state.outputText[this.state.outputText.length-1] === '*' ) {
+      let result = this.state.outputText.slice(0, this.state.outputText.length-1)
+      console.log(result)
+      this.setState({outputText: result});
+    }
+    // console.log(this.state.outputText)
+    console.log(result)
+    let result = eval(this.state.outputText);
+    document.getElementById('input').value = result;
+    this.setState({outputText: result });    
   }  
 
+  delete () {
+    let removedLast = this.state.outputText.slice(0,this.state.outputText.length-1);
+    console.log(removedLast)
+    this.setState({outputText: removedLast});
+    document.getElementById('input').value = removedLast;
+
+  }
 
   render() {
     return (
-      <div>
-        <input type='text' id='input' />
-          <div>
-          <button className='button' id='one' onClick={()=>this.addText('1')}>1</button>
-          <button className='button' id='two' onClick={()=>this.addText('2')}>2</button>
-          <button className='button' id='three' onClick={()=>this.addText('3')}>3</button>
-          <button className='button' id='AC' onClick={()=>this.AC('AC')}>AC</button>
-
-          </div>
-          <div>
-          <button className='button' id='four' onClick={()=>this.addText('4')}>4</button>
-          <button className='button' id='five' onClick={()=>this.addText('5')}>5</button>
-          <button className='button' id='six' onClick={()=>this.addText('6')}>6</button>
-          <button className='button' id='add' onClick={()=>this.addText('+')}>+</button>
-
-          </div>
-          <div>
-          <button className='button' id='seven' onClick={()=>this.addText('7')}>7</button>
-          <button className='button' id='eight' onClick={()=>this.addText('8')}>8</button>
-          <button className='button' id='nine' onClick={()=>this.addText('9')}>9</button>
-          <button className='button' id='subtract' onClick={()=>this.addText('-')}>-</button>
-
-          </div>
-          <div>
-          <button className='button' id='zero' onClick={()=>this.addText('0')}>0</button>
-          <button className='button' id='divide' onClick={()=>this.addText('/')}>/</button>
-          <button className='button' id='multiply' onClick={()=>this.addText('*')}>x</button>
-          <button className='button' id='equals' onClick={()=>this.equals(document.getElementById('input').value)}>=</button>
-
-          </div>
+      <div id='main'>
+        <input type='text' id='input' ></input>
+        <table id='table'>
+          <tr>
+            <td><button className='button' id='one' onClick={()=>this.addText('1')}>1</button> </td>
+            <td><button className='button' id='two' onClick={()=>this.addText('2')}>2</button></td>
+            <td><button className='button' id='three' onClick={()=>this.addText('3')}>3</button></td>
+            <td><button className='button' id='AC' onClick={()=>this.AC('AC')}>AC</button></td>
+          </tr>
+          <tr>
+            <td><button className='button' id='four' onClick={()=>this.addText('4')}>4</button></td>
+            <td><button className='button' id='five' onClick={()=>this.addText('5')}>5</button></td>
+            <td><button className='button' id='six' onClick={()=>this.addText('6')}>6</button></td>
+            <td><button className='button' id='add' onClick={()=>this.addText('+')}>+</button></td>
+          </tr>
+          <tr>
+            <td><button className='button' id='seven' onClick={()=>this.addText('7')}>7</button></td>
+            <td><button className='button' id='eight' onClick={()=>this.addText('8')}>8</button></td>
+            <td><button className='button' id='nine' onClick={()=>this.addText('9')}>9</button></td>
+            <td><button className='button' id='subtract' onClick={()=>this.addText('-')}>-</button></td>
+          </tr>
+          <tr>
+            <td><button className='button' id='zero' onClick={()=>this.addText('0')}>0</button></td>
+            <td><button className='button' id='divide' onClick={()=>this.addText('/')}>/</button></td>
+            <td><button className='button' id='multiply' onClick={()=>this.addText('*')}>x</button></td>
+            <td><button className='button' id='equals' onClick={()=>this.equals()}>=</button></td>
+          </tr>
+          <tr>
+            <td></td>
+            <td><button className='button' id='dot' onClick={()=>this.addText('.')}>.</button></td>
+            <td><button className='button' id='backSpace' onClick={()=>this.delete()}>Del</button></td>
+            <td></td>
+          </tr>
+          
+        </table>
       </div>
     );
   }
